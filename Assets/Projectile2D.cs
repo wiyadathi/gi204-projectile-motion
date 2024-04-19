c26be9bfbcd8c6847da68950a040eef31da5fd59
wiyadathi <wiyada.t@bu.ac.th>
2024-04-19 16:35:09 +0700

    projectile 2D finished

--- Assets/Projectile2D.cs
+++ Assets/Projectile2D.cs
@@ -12,18 +12,42 @@ public class Projectile2D : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //shoot raycast to the mouse click position
+            //shoot raycast to detect the mouse click position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(ray.origin, ray.direction * 100f, Color.magenta, 100);
+            Debug.DrawRay(ray.origin, ray.direction * 10f, Color.magenta, 100);
 
             //get the position of mouse click
             RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
 
             if (hit.collider != null)
             {
+                //move the target to the mouse clicked position
                 target.transform.position = new Vector2(hit.point.x, hit.point.y);
                 Debug.Log("hit: " + hit.point);
+                
+                //calculate velocity of the projectile motion
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+                
+                //fire bullet using Rigidbody
+                Rigidbody2D firedBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+
+                //add velocity to bullet's rigidbody 
+                firedBullet.velocity = projectileVelocity;
             }
         }
+    }
+
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+    {
+        Vector2 distance = target - origin;
+
+        //find velocity on x and y axis
+        float velocityX = distance.x / time;
+        float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+        
+        //get new projectile vector
+        Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
+        
+        return projectileVelocity;
     }
 }
